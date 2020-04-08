b4eb90fdc309a5393a5fd70912a81b2515da93c1
Klaudynaaa <63228910+Klaudynaaa@users.noreply.github.com>
2020-04-08 13:00:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -22,33 +22,54 @@ if ($conn->connect_error) {
 
 echo "connected successfully";
 
-
-if(isset($_POST['but_upload'])){
-//    $name = $_FILES['file']['name'];
-    $name = $_POST['title'];
-    $target_dir = "upload/";
-    $target_file = $target_dir . basename($_FILES["file"]["name"]);
-
-    // Select file type
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-    // Valid file extensions
-    $extensions_arr = array("jpg","jpeg","png","gif");
-
-    // Check extension
-    if( in_array($imageFileType,$extensions_arr) ){
-
-        // Convert to base64
-//        $image_base64 = base64_encode(file_get_contents($_FILES['file']['tmp_name']) );
-//        $image = 'data:image/'.$imageFileType.';base64,'.$image_base64;
+$name = $_POST['title'];
+$target_dir = "upload/";
+$target_file = $target_dir . basename($_FILES["file"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["file"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["file"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
 
         // Insert record
         $query = "insert into demotywatory(title,imgsrc) values('".$name."','".$target_file."')";
 
         mysqli_query($conn,$query) or die(mysqli_error($conn));
 
+    } else {
+        echo "Sorry, there was an error uploading your file.";
     }
-
 }
 
 $sql = "select * from demotywatory order by id desc ";
@@ -102,7 +123,7 @@ $conn->close();
             <h3><?php echo $r['title']?></h3>
             <img src='<?php echo $r['imgsrc'] ?>' >
 
-            <button id="<?php echo $r['id'] ?>" onClick="reply_click(this)">Usun</button>
+            <button class="btnpost" id="<?php echo $r['id'] ?>" onClick="reply_click(this)">Usun</button>
         </div>
         <?php endforeach; ?>
     </div>
